c995c121a75b40b40de51490d720d7fa2afdb057
jze9 <118809237+jze99@users.noreply.github.com>
2024-10-22 22:57:02 +0500

    start add images

--- page/order_page.py
+++ page/order_page.py
@@ -1,5 +1,5 @@
 import flet as ft
-from designer import Designer,TextField, dialog
+from designer import Designer,TextField, dialog, file_picer
 import user_data
 import requests
 
@@ -20,6 +20,20 @@ class order_page():
         self.Categorial_order = TextField(value=order_page.Categorial_page)
         self.Description_order = TextField(value=order_page.Description_page)
         self.status_order = order_page.status_page
+        self.file_picer = file_picer(on_result=self.file_picer_result)
+        self.images = ft.Container(
+            height=200,
+            width=200,
+            border_radius=ft.border_radius.all(10),
+            bgcolor=Designer.colors[0],
+            on_click=lambda _: self.file_picer.pick_files(
+                allowed_extensions=["png", "csv", "jpg", "jpeg"],
+                file_type=ft.FilePickerFileType.CUSTOM,
+                allow_multiple=False,
+            ),
+        )
+        self.page.overlay.append(self.file_picer)
+        self.path_images:str=""
         self.page_view=ft.Column(
             expand=True,
             controls=[
@@ -54,12 +68,7 @@ class order_page():
                         ft.Column(
                             horizontal_alignment=ft.CrossAxisAlignment.CENTER,
                             controls=[
-                                ft.Container(
-                                    height=200,
-                                    width=200,
-                                    border_radius=ft.border_radius.all(10),
-                                    bgcolor=Designer.colors[0]
-                                ),
+                                self.images,
                                 ft.Row(
                                     controls=[
                                         ft.Text(value="Name:", size=22,color=Designer.colors[4]),
@@ -116,7 +125,9 @@ class order_page():
     def save_product(self, e):
         if self.status_order ==  "Выставлен":
             if self.name_order.value != "" and self.price_order.value != "" and self.Categorial_order.value != "" and self.Description_order.value != "":
-                if self.price_order.value.isnumeric():
+                temp_price = str(self.price_order.value)
+                if temp_price.isnumeric():
+                    self.load_images()
                     r = requests.put("http://31.31.196.6:8000/ozito/update_product?id="+str(self.p_id)+
                             "&product_name="+self.name_order.value+"&product_description="+
                             self.Description_order.value+"&price="+self.price_order.value+"&creator_id="+str(self.c_id)
@@ -128,6 +139,30 @@ class order_page():
                 self.page.open(dialog(text="All fields must be filled."))
         else:
             self.page.open(dialog(text="Order can't be updated since it was bought."))
+    
+    def file_picer_result(self,e):
+        if e.files:
+            self.path_images = e.files[0].path
+            self.images.image = ft.DecorationImage(src=e.files[0].path)
+            self.images.update()
+    
+    def load_images(self):
+        from ftplib import FTP
+        # Укажите ваши данные для подключения
+        ftp_host = '31.31.196.6'  # адрес FTP-сервера
+        ftp_user = 'u2806602'     # ваше имя пользователя
+        ftp_pass = 'wE8aC4bF4uiZ7vL8'      # ваш пароль
+        # Путь, куда вы хотите загрузить изображение на сервере
+        remote_image_path = 'www/ozito.ru/images/'
+        ftp = FTP(ftp_host)
+        ftp.login(ftp_user, ftp_pass)
+
+        # Открываем файл и загружаем его на сервер
+        with open(self.path_images, 'rb') as image_file:
+            ftp.storbinary('STOR ' + f"{remote_image_path}{self.c_id}", image_file)  # Замените 'картинка.jpg' на нужное имя файла на сервере
+
+        # Закрываем соединение
+        ftp.quit()
     
     def go_to_orders(self,e):
         self.page.go("/my_orders")
